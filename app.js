7e5509f8a723b67f262b40d8fd4a2e07dd0a3abf
Ian <mus5002@aol.com>
2018-02-07 08:28:32 -0500

    moved body parser

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const db = require('./db')
 
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+
 const UserController = require('./controllers/UserController')
 app.use('/users', UserController)
 
